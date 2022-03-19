c5706c6904dcf9038cb45f984ae64aecec7ef513
TOVTC <veronica.tc.to@outlook.com>
2022-03-19 15:42:29 -0400

    completed task retrieval from local storage and assigned tasks to respective status columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,7 +156,7 @@ var deleteTask = function(taskId) {
         }
     }
     //reassign tasks array to be the same as updatedTaskArr
-    tasks = updatedTaskArr;
+    tasks = updatedTasksArr;
     //update tasks array
     saveTasks();
 };
@@ -189,6 +189,44 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function() {
+    tasks = localStorage.getItem("tasks", tasks);
+    console.log(tasks);
+    if (!tasks) {
+        tasks = [];
+        return false;
+    }
+    tasks = JSON.parse(tasks);
+    console.log(tasks);
+    for (i = 0 ; i < tasks.length; i++) {
+        tasks[i].id = taskIdCounter;
+        console.log(tasks[i]);
+        var listItemEl = document.createElement ("li");
+        listItemEl.className = "task-item";
+        listItemEl.setAttribute ("data-task-id", tasks[i].id);
+        console.log(listItemEl);
+        var taskInfoEl = document.createElement("div")
+        taskInfoEl.className = "task-info";
+        taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+        listItemEl.appendChild(taskInfoEl);
+        var taskActionsEl = createTaskActions(tasks[i].id);
+        listItemEl.appendChild(taskActionsEl);
+        console.log(listItemEl);
+        if (tasks[i].status === "to do") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        } else if (tasks[i].status === "in progress") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        } else if (tasks[i].status === "complete") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+        taskIdCounter++
+        console.log(listItemEl);
+    }
+}
+
 //"click" cannot be used as the event because the event listener is assigned to the entire form, so "submit" is used instead for click and enter
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -197,3 +235,6 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 //add an event listener to check for when select dropdown changes values
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+//call load tasks
+loadTasks();
